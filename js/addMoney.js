5c3a6b9f9b4249657138956d52166831cb50cd8d
prantonub <prantonub.cse23@gmail.com>
2025-07-18 01:54:56 +0600

    add money function added

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,1 +1,21 @@
-console.log('add money')
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+event.preventDefault();
+
+const addMoney = document.getElementById('input-add-money').value;
+const addMoneyNumber = parseFloat(addMoney);
+const pinNumber = document.getElementById('input-pin-number').value;
+
+console.log(addMoney);
+console.log(pinNumber);
+
+if(pinNumber == 1234){
+    const accountBalance = document.getElementById('account-balance').innerText;
+    const balanceNumber = parseFloat(accountBalance);
+
+    const newBalance = addMoneyNumber + balanceNumber;
+    document.getElementById('account-balance').innerText = newBalance;
+}
+else{
+    alert('failed to add money')
+}
+})
